7430651801d430ef5e80e65735af091952fa195e
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-07-26 23:55:43 +0200

    copy sh

---
+++ ffgh_xbar_plugin.10s.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# <xbar.title>ffgh</xbar.title>
+# <xbar.version>v1.0.0</xbar.version>
+# <xbar.author>Jakub Mikians</xbar.author>
+# <xbar.author.github>jakub-m</xbar.author.github>
+# <xbar.desc>GitHub PR summary</xbar.desc>
+
+ffgh-bin show-compact-summary
